8c45c04210c9140cb6fae74a891e61de35bdadc2
nitrocode <nitrocode@users.noreply.github.com>
2021-05-25 17:30:35 -0400

    Change output try() to return empty map if failed

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "inspector_assessment_target" {
   description = "The AWS Inspector assessment target"
-  value       = try(aws_inspector_assessment_target.target[0], "")
+  value       = try(aws_inspector_assessment_target.target[0], {})
 }
 
 output "aws_inspector_assessment_template" {
   description = "The AWS Inspector assessment template"
-  value       = try(aws_inspector_assessment_template.assessment[0], "")
+  value       = try(aws_inspector_assessment_template.assessment[0], {})
 }
 
 output "aws_cloudwatch_event_rule" {
   description = "The AWS Inspector event rule"
-  value       = try(aws_cloudwatch_event_rule.schedule[0], "")
+  value       = try(aws_cloudwatch_event_rule.schedule[0], {})
 }
 
 output "aws_cloudwatch_event_target" {
   description = "The AWS Inspector event target"
-  value       = try(aws_cloudwatch_event_target.target[0], "")
+  value       = try(aws_cloudwatch_event_target.target[0], {})
 }
